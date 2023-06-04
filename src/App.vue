50e617170056889b4698b7ad79c52b2f4a953071
Do0oMores <ws2004421@outlook.com>
2023-06-05 01:05:53 +0800

    create

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <transition name="fade">
+    <!-- 导航栏 -->
+      <img v-if="showImage" class="fullscreen-image" src="../public/data/OPENPIC.jpg" alt="Animated Image">
+    </transition>
+    <button @click="begin" class="rounded-button">BEGIN</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import openPic from "@/assets/OPENPIC.jpg";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      showImage: false,
+      openIMG: true,
+      openPic: openPic
+    };
+  },
+  mounted(){
+    setTimeout(() => {
+      this.showImage=true;
+    }, 500);
+  },
+  /*
+  components:{
+    Header
   }
-}
+  */
+};
 </script>
 
 <style>
+/* 添加样式来美化页面 */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px;
 }
+
+nav ul {
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  padding: 0;
+}
+
+nav li {
+  margin-right: 10px;
+}
+
+nav a {
+  text-decoration: none;
+  color: rgb(54, 114, 54);
+}
+
+main {
+  margin-top: 20px;
+}
+
+button {
+  margin-top: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  font-size: 24px;
+  padding: 12px 24px;
+}
+
+.fullscreen-image {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.rounded-button{
+  border: none;
+  border-radius: 50%;
+  padding: 12px 24px;
+  font-size: 24px;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.rounded-button:hover {
+  background-color: #0056b3;
+}
+
 </style>
+
